e69600c1ba41d2c5e834d2f4924304571a87a1c2
Dominik <djessen94@gmail.com>
2023-09-11 20:16:25 +0200

    Animated waving hand emoji

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,19 @@ module.exports = {
         foreground: 'hsl(78 71% 3%)',
         primary: 'hsl(13 58% 63%)',
         accent: 'hsl(158 49% 39%)'
+      },
+      keyframes: {
+        wave: {
+          '0%, 75%, 100%': { transform: 'rotate(0deg)' },
+          '12.5%': { transform: 'rotate(16deg)' },
+          '25%': { transform: 'rotate(-10deg)' },
+          '37.5%': { transform: 'rotate(16deg)' },
+          '50%': { transform: 'rotate(-10deg)' },
+          '62.5%': { transform: 'rotate(16deg)' }
+        }
+      },
+      animation: {
+        wave: 'wave 2s ease-in-out infinite'
       }
     }
   },
