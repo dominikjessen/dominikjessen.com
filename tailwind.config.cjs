cff5dcf6156b6b8347a9f056668df465070120fb
Dominik <djessen94@gmail.com>
2023-09-11 16:27:45 +0200

    Brand colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        background: 'hsl(52 94% 94%)',
+        foreground: 'hsl(78 71% 3%)',
+        primary: 'hsl(13 58% 63%)',
+        accent: 'hsl(158 49% 39%)'
+      }
+    }
+  },
+  plugins: []
+};
