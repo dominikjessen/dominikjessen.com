eecbe476f666323c0444fd24f6ee219e4e1a0510
Dominik <djessen94@gmail.com>
2023-09-19 10:29:06 +0200

    Remove unused components

--- src/components/Card.tsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ title, textColor }: { title: string; textColor?: string }) {
-  return (
-    <div className="w-52 h-80 p-12 bg-fuchsia-300 rounded">
-      <h2 className={`font-bold ${textColor}`}>{title}</h2>
-    </div>
-  );
-}
